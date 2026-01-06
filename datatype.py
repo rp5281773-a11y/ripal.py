70f6cb5858a5934b1533e7ca9b11d1d21cb3d85f
rp5281773-a11y <rp5281773@gmail.com>
2026-01-06 08:02:13 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,34 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("hello")
+hello
+x=3
+print(x)
+3
+x=20.5
+print(x)
+20.5
+x=1j
+print(x)
+1j
+x=["apple","banana"]
+print(x)
+['apple', 'banana']
+x=("apple","banana")
+print(x)
+('apple', 'banana')
+x=range(6)
+print(x)
+range(0, 6)
+x={"name","john","age":21}
+SyntaxError: invalid syntax
+x=True
+print(X)
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    print(X)
+NameError: name 'X' is not defined. Did you mean: 'x'?
+NameError: name 'X' is not defined. Did you mean: 'x'?
+SyntaxError: invalid syntax
+SyntaxError: invalid syntax\
+             XX
