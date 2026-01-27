2c1abff60103484353b5b95865416ad9d1e78bcc
rp5281773-a11y <rp5281773@gmail.com>
2026-01-27 08:15:38 +0530

    Add files via upload

---
+++ arithmetic operatar.py
@@ -0,0 +1,77 @@
+Python 3.7.9 (tags/v3.7.9:13c94747c7, Aug 17 2020, 18:01:55) [MSC v.1900 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("hello")
+hello
+>>> a=5
+>>> b=10
+>>> print(a+b)
+15
+>>> print(a-b)
+-5
+>>> print(a/b)
+0.5
+>>> x=10
+>>> print(x)
+10
+>>> a=5
+>>> print(-a)
+-5
+>>> p=10
+>>> a=5
+>>> print(p>a)
+True
+>>> print(p==a)
+False
+>>> x=true
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    x=true
+NameError: name 'true' is not defined
+>>> print(x and y)
+Traceback (most recent call last):
+  File "<pyshell#15>", line 1, in <module>
+    print(x and y)
+NameError: name 'y' is not defined
+>>> x=True
+>>> y=False
+>>> print(xandy)
+Traceback (most recent call last):
+  File "<pyshell#18>", line 1, in <module>
+    print(xandy)
+NameError: name 'xandy' is not defined
+>>> print(x and y)
+False
+>>> print(x or y)
+True
+>>> print(not x)
+False
+>>> is_pass= True
+>>> print(is_pass)
+True
+>>> a=5
+>>> b=3
+>>> print(a&b)
+1
+>>> print(a/b)
+1.6666666666666667
+>>> numbers=[1,2,3,4}
+SyntaxError: invalid syntax
+>>> numbet={1,2,3,4]
+SyntaxError: invalid syntax
+>>> numbers[1,2,3,4]
+Traceback (most recent call last):
+  File "<pyshell#30>", line 1, in <module>
+    numbers[1,2,3,4]
+NameError: name 'numbers' is not defined
+>>> numbers=[1,2,3,4]
+>>> print(2 in numbers)
+True
+>>> print(5 not in numbers)
+True
+>>> x=10
+>>> y=10
+>>> print(x is y)
+True
+>>> print(x is not y)
+False
+>>> 
