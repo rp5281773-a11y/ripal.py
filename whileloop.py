198a81a3961729a3af3d4a8eff5bde28b3c46f90
rp5281773-a11y <rp5281773@gmail.com>
2026-02-24 09:10:48 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,19 @@
+i=1
+while i<=10:
+    print(i)
+    i=i+1
+n=int(input("enter n:"))
+i=1
+s=0
+while i<=n:
+    s=s+i
+    i=i+1
+    print("sum=",s)
+num=int(input("enternumber:"))
+i=1
+while i<=10:
+    printf(num,"x",i,"=",num*i)
+    i=i+1
+    
+
+    
